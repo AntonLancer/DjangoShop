2082867b0f5c14025a4c5f3b03b5a4619c1651cb
anton <anton@admin.com>
2025-08-12 09:35:27 +0300

    init project and create models

---
+++ main/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True) # Человекочитательный код для URL
+
+    # Создание уникального slug при сохранении модели
+    def save (self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    # Отображение имени категории в админке
+    def __str__(self):
+        return self.name
+    
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+class Product_Size(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
+    size = models.ForeignKey('Size', on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.size.name} ({self.stock} in stock) for {self.product.name}"  
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products') # Наследование другой модели
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    main_image = models.ImageField(upload_to='products/main/')
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Создание уникального slug при сохранении модели
+    def save (self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/extra/')
